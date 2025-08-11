26293affc937691fd34efaac82369f29b3c82f94
DeadZerok <ivarona@unimayor.edu.co>
2025-08-11 11:12:29 -0500

    feat(services): actualizar página y añadir nuevos componentes
    - Modifica contenido y estilos en services/page.tsx- Ajusta estilos globales en globals.css y estructura en layout.tsx- Añade nuevos componentes: AvatarService, CircleImage y SliderServices- Actualiza configuración en .gitignore- Actualiza dependencias en package.json y package-lock.json

---
+++ components/avatar-service.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { MotionTransition } from "./transitions-componets";
+
+const AvatarServices = () => {
+    return (
+        <MotionTransition position="right"
+            className="bottom-0 left-0 hidden md:inline-block md:absolute">
+                <Image src="/services.png" width={300} height={300} className="w-[350px] h-full" alt="Avatar"/>
+        </MotionTransition>
+    );
+}
+
+export default AvatarServices;

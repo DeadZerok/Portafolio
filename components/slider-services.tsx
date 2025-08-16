a7eb8b6ebce72143b020f1fdff3d544442483fc0
DeadZerok <ivanarley0398@gmail.com>
2025-08-16 12:06:27 -0500

    feat(services): ajuste posicion de slider
    - posicion y tamaño de slider
    - centrado de iconos

--- components/slider-services.tsx
+++ components/slider-services.tsx
@@ -21,7 +21,7 @@ const SliderServices = () => {
                 },
                 1024: {
                     slidesPerView: 3,
-                    spaceBetween: 15
+                    spaceBetween: 20
                 }
             }}
             freeMode={true}
@@ -30,12 +30,13 @@ const SliderServices = () => {
                 clickable: true
             }}
             modules={[Pagination]}
-            className="h-[280px] md:h-[340px] w-[270px] md:w-[550px]"
+            className="h-[280px] md:h-[340px] w-[270px] md:w-[650px] "
+            
         >
 
             {serviceData.map((item, index) => (
                 <SwiperSlide key={index} >
-                    <div className="flex px-6 py-8  h-auto md:h-[290px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col gap-x-6 sm:gap-x-0 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-secondary border-2">
+                    <div className="flex px-6 py-8  h-auto md:h-[290px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] flex-col gap-x-6 sm:gap-x-0 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-secondary border-2">
                         <div className="mb-4 text-4xl text-secondary justify-items-center">{item.icon}</div>
                         <div>
                             <h3 className="mb-4 text-lg">{item.title}</h3>

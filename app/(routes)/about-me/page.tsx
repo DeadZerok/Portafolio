696258bac7acce27ad88dfc88afdbf04e8eb70f4
DeadZerok <ivanarley0398@gmail.com>
2025-08-28 15:28:12 -0500

    Feat(about me)

--- app/(routes)/about-me/page.tsx
+++ app/(routes)/about-me/page.tsx
@@ -12,7 +12,7 @@ const pageAboutMe = () => {
             <ContainerPage>
                 <Avatar />
                 <h1 className="text-2xl leading-tight text-center md:text-left md:text-5xl md:mt-10">Toda mi{' '}
-                    <span className="font-bold text-secondary">Trayectoria profecional</span>
+                    <span className="font-bold text-secondary">Trayectoria profesional</span>
                 </h1>
 
                 <ConunterServices />
